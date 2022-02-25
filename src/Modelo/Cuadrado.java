6280b376255410ba382b4940e2fbc3e0c3333a7d
juanfaldana20 <98820412+juanfaldana20@users.noreply.github.com>
2022-02-25 16:06:49 -0500

    Cambios
    En modelo cree dos clases: Punto2D y cuadrados.

---
+++ src/Modelo/Cuadrado.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author juan_fernando.aldana
+ */
+public class Cuadrado extends FigurasGeometricas{
+    
+    private double Lado;
+
+    /**
+     * Get the value of Lado
+     *
+     * @return the value of Lado
+     */
+    public double getLado() {
+        return Lado;
+    }
+
+    /**
+     * Set the value of Lado
+     *
+     * @param Lado new value of Lado
+     */
+    public void setLado(double Lado) {
+        this.Lado = Lado;
+    }
+
+}
